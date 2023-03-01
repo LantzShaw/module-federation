d900ab8778d238b22504c4d4e5f1566232652b2c
Lantz <lantzshaw@163.com>
2023-03-01 09:48:17 +0800

    initialize project

---
+++ app1/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Page Header {{ name }}</h1>
+    <h2>Header</h2>
+    <Test />
+  </div>
+</template>
+<script>
+import Test from './Test.vue'
+export default {
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+  },
+  components: { Test },
+}
+</script>
+<style></style>
